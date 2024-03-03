9e89185da5cc330e77332bd6c901fa7beffe8e62
Jociano Perin <jociano.perin@outlook.com>
2024-03-03 10:24:41 -0300

    Ajustes e teste tensowflow

---
+++ tensorflowTeste.py
@@ -0,0 +1,7 @@
+# Execute em um ambiente Python
+import tensorflow as tf
+
+if tf.test.gpu_device_name():
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+else:
+    print("Please install GPU version of TF")
